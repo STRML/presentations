e2a543af0dc4a0a453755348f661725a65a9014c
Samuel Reed <sam@tixelated.com>
2014-05-21 11:56:04 +0800

    Add velocity / scuttlebutt demo

---
+++ examples/8 wat/browser.js
@@ -0,0 +1,19 @@
+var shoe = require('shoe');
+var sockStream = shoe('/sock');
+
+var Model = require('scuttlebutt/model');
+var model = new Model();
+
+sockStream.pipe(model.createStream()).pipe(sockStream);
+
+var setPerspective = window.setPerspective;
+window.setPerspective = function(x, y) {
+  console.log('setting position', arguments);
+  model.set('position', {x: x, y: y});
+  setPerspective.apply(this, arguments);
+};
+
+model.on('update', function(data){
+  var key = data[0], value = data[1];
+  if (key === 'position') setPerspective(value.x, value.y);
+});
